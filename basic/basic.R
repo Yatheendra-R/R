b77ad8fa02c0a9fea0cd5bcd3560831f00b55e0d
Yatheendra-R <yatheendra.r07@gmail.com>
2026-04-11 00:13:21 +0530

    Basic

---
+++ basic/basic.R
@@ -0,0 +1,42 @@
+x=20
+X<-25
+x+X
+round(1.23)
+3+2
+2-3
+2*3
+2**3
+25**0.5
+25^0.5
+l=c(1,2,3,4,5)
+l
+sum(l1)
+prod(l1)
+l^2
+l*3
+l1=c(2,3)
+l^l1
+l %/% l1
+max(1.2,4.4,-7.8)
+min(c(1.2,4.4,-7.8))
+l2=c(-2,-3,-4)
+abs(l2)
+sqrt(c(4,9,16))
+
+log(10) #ln(10)
+log10(10)
+log(9, base=3)
+c=3+5i
+Re(c)
+Im(c)
+Conj(c)
+Mod(c)
+Arg(c)
+mat=matrix(nrow=4,ncol=2,data=c(1,2,3,4,5,6,7,8))
+mat[3,2]
+dim(mat)
+nrow(mat)
+ncol(mat)
+tmat=t(mat)
+tmat
+iden=diag(1,nrow=2,ncol=2)
